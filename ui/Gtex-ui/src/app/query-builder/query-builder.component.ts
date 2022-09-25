f1cc55645fbd5b9b4c092840adf6ac8d6f66ff42
Rocco <rocco.scicchitano@gpi.it>
2022-09-25 16:54:13 +0200

    Corretta visualizzazione della tabella, aggiunta funzionalità per download in excel.

--- ui/Gtex-ui/src/app/query-builder/query-builder.component.ts
+++ ui/Gtex-ui/src/app/query-builder/query-builder.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 
+import * as XLSX from 'xlsx';
+
 @Component({
   selector: 'app-query-builder',
   templateUrl: './query-builder.component.html',
@@ -522,7 +524,21 @@ export class QueryBuilderComponent implements OnInit {
     else{
       alert("Select at least one tissue!");
     }
-    
   }
+
+  exportexcel(): void 
+    {
+       /* table id is passed over here */   
+       let element = document.getElementById('tableQuery'); 
+       const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+
+       /* generate workbook and add the worksheet */
+       const wb: XLSX.WorkBook = XLSX.utils.book_new();
+       XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+       /* save to file */
+       XLSX.writeFile(wb, this.tessutoVisualizzato+".xlsx");
+			
+    }
 
 }
